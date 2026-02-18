6fa057671e30f29736f69e9f25adabc27a5c65d2
gwen041 <almiratabios01@gmail.com>
2026-02-18 23:18:22 +0800

    Add: click menu

---
+++ Website14/script.js
@@ -0,0 +1,4 @@
+function showMenu() {
+      const dropDown = document.getElementById('dropd');
+      dropDown.style.display === 'flex' ? dropDown.style.display = 'none' : dropDown.style.display = 'flex';
+}
